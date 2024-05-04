8e6606df0f0146a3dc2d940fa875fc1f66e41d92
spacescape <15368104568@189.cn>
2024-05-04 22:23:41 +0800

    Add files via upload

---
+++ Colemak.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+void colemak() {
+    int Q = 27;
+    int W = 28;
+    int F = 29;
+    int P = 30;
+    int G = 31;
+
+    int J = 32;
+    int L = 33;
+    int U = 34;
+    int Y = 35;
+    int A = 36;
+
+    int R = 37;
+    int S = 38;
+    int T = 39;
+    int D = 40;
+    int H = 45;
+
+    int N = 46;
+    int E = 47;
+    int I = 48;
+    int O = 49;
+    int Z = 50;
+
+    int X = 51;
+    int C = 52;
+    int V = 53;
+    int B = 54;
+    int K = 55;
+
+    int M = 56;
+    printf("%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p\n",
+                   &Q, &W, &F, &P, &G, &J, &L, &U, &Y, &A, &R, &S, &T, &D, &H, &N, &E, &I, &O, &Z, &X, &C, &V, &B, &K, &M);
+}
+void colemak2(){
+    int q = 1;
+    int w = 2;
+    int f = 3;
+    int p = 4;
+    int g = 5;
+
+    int j = 6;
+    int l = 7;
+    int u = 8;
+    int y = 9;
+    int a = 10;
+
+    int r = 11;
+    int s = 12;
+    int t = 13;
+    int d = 14;
+    int h = 15;
+    int n = 16;
+    int e = 17;
+    int i = 18;
+    int o = 19;
+    int z = 20;
+
+    int x = 21;
+    int c = 22;
+    int v = 23;
+    int b = 24;
+    int k = 25;
+
+    int m = 26;
+    printf("%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p\n",
+           &q, &w, &f, &p, &g,&j, &l, &u, &y, &a, &r, &s, &t, &d, &h, &n, &e, &i, &o, &z, &x, &c, &v, &b, &k, &m);
+}
+
+
+
---
+++ dvorak.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+void dovorak(){
+    int p = 1;
+    int y = 2;
+    int f = 3;
+    int g = 4;
+    int c = 5;
+
+    int r = 6;
+    int l = 7;
+    int a = 8;
+    int o = 9;
+    int e = 10;
+
+    int u = 11;
+    int i = 12;
+    int d = 13;
+    int h = 14;
+    int t = 15;
+
+    int n = 16;
+    int s = 17;
+    int q = 18;
+    int j = 19;
+    int k = 20;
+
+    int x = 21;
+    int b = 22;
+    int m = 23;
+    int w = 24;
+    int v = 25;
+
+    int z = 26;
+    printf("%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p\n",
+           &p, &y, &f, &g, &c, &r, &l, &a, &o, &e, &u, &i, &d, &h, &t, &n, &s, &q, &j, &k, &x, &b, &m, &w, &v, &z);
+    return dovorak();
+}
+
+int dovorak2(){
+    int  P = 27;
+    int  Y = 28;
+    int  F = 29;
+    int  G = 30;
+    int  C = 31;
+
+    int  R = 32;
+    int  L = 33;
+    int  A = 34;
+    int  O = 35;
+    int  E = 36;
+
+    int  U = 37;
+    int  I = 38;
+    int  p = 39;
+    int  H = 40;
+    int  T = 45;
+
+    int  N = 46;
+    int  S = 47;
+    int  Q = 48;
+    int  J = 49;
+    int  K = 50;
+
+    int  X = 51;
+    int  B = 52;
+    int  M = 53;
+    int  W = 54;
+    int  V = 55;
+
+    int  Z = 56;
+      printf("%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p,%p\n",
+            &P,&Y,&F,&G,&C,&R,&L,&A,&O,&E,&U,&I，&H,&T,&N,&S,&Q,&J,&K,&X,&B,&M,&W,&V,&Z);
+    return dovorak2();
+}
+
+
+
